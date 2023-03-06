7c2ddc440ae833c8dc2e371506ba0bfd1d58e7b2
Piero Coronado <pacoronado01@gmail.com>
2023-03-05 20:24:07 -0500

    Refactor code to fetch standings based on leagueID and season

--- public/scripts/rankings.js
+++ public/scripts/rankings.js
@@ -4,10 +4,13 @@ const rankingsContainer = document.querySelector("#rankings-ctn");
 const viewFixturesLink = document.querySelector("#viewfixtures");
 const url = window.location.href;
 const urlParts = url.split("/");
-const leagueNameShort = urlParts[urlParts.length - 1];
+const leagueNameShort = urlParts[urlParts.length - 2];
+const leagueSeason = urlParts[urlParts.length - 1];
 
 // Fetch the rankings for the specified league
-fetch(`http://localhost:3000/football/${leagueNameShort}/standings`)
+fetch(
+	`http://localhost:3000/football/${leagueNameShort}/standings/${leagueSeason}`
+)
 	.then((response) => {
 		return response.json();
 	})
@@ -19,17 +22,26 @@ fetch(`http://localhost:3000/football/${leagueNameShort}/standings`)
 	});
 
 function displayRankings(data) {
-	const { leagueName, leagueID, leagueSeason, leagueStandings } = data;
+	const { leagueInfo, standings, teamsInfo } = data;
+	const { leagueID, leagueName, leagueCountry, leagueLogo } = leagueInfo;
+	const { leagueSeason, leagueStandings } = standings;
+
 	leagueNameHeading.innerText = leagueName;
 	rankingsHeading.innerText = `${leagueName} Rankings ${leagueSeason} Season`;
 	viewFixturesLink.setAttribute(
 		"href",
 		`/football/${leagueNameShort}/fixtures`
 	);
+
+	// Parse through the leagueStandings obj
 	for (let team of leagueStandings) {
-		const { teamID, teamName, teamPoints, teamRanking } = team;
+		const { teamID, teamPoints, teamRanking } = team;
+		const teamInformation = teamsInfo.find((obj) => teamID == obj.teamID); // find the obj with matching teamID
+		const { teamName, teamCode } = teamInformation;
+
+		//Create the paragraph element
 		const p = document.createElement("p");
-		p.innerText = `${teamRanking}. ${teamName} - ${teamPoints}`;
+		p.innerText = `${teamRanking}. ${teamName} (${teamCode}) - ${teamPoints}`;
 		rankingsContainer.appendChild(p);
 	}
 }
