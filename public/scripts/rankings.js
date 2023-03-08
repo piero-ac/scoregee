00590606d9f19ff4e4aad4e2ea6a3bac5d58e6b1
Piero Coronado <pacoronado01@gmail.com>
2023-03-08 17:56:00 -0500

    Fix typo and add log messages

--- public/scripts/rankings.js
+++ public/scripts/rankings.js
@@ -2,8 +2,9 @@ const leagueNameHeading = document.querySelector("#leagueName-hd");
 const leagueSeasonHeading = document.querySelector("#leagueSeason-hd");
 const rankingsHeading = document.querySelector("#rankings-hd");
 const rankingsContainer = document.querySelector("#rankings-ctn");
-const viewFixturesLink = document.querySelector("#viewfixtures");
+// const viewFixturesLink = document.querySelector("#viewfixtures");
 const fixtureDatesDiv = document.querySelector("#fixture-dates-div");
+const fixturesForDateDiv = document.querySelector("#fixtures-for-date");
 const fixtureDisplayDateHeading = document.querySelector(
 	"#fixture-display-date-hd"
 );
@@ -44,11 +45,6 @@ function parseData(data) {
 
 	const season = `${leagueSeason}-${parseInt(leagueSeason) + 1} Season`;
 	leagueSeasonHeading.innerText = season;
-	// rankingsHeading.innerText = `${leagueName} Rankings ${leagueSeason} Season`;
-	viewFixturesLink.setAttribute(
-		"href",
-		`/football/${leagueNameShort}/fixtures/${leagueSeason}`
-	);
 
 	// Parse through the leagueStandings obj
 	for (let team of leagueStandings) {
@@ -81,6 +77,7 @@ function displayFixtureDates(fixtures) {
 
 		// add event listener to display button value
 		button.addEventListener("click", function () {
+			fixturesForDateDiv.textContent = "";
 			displayFixturesForDate(this.value, this.innerText);
 		});
 		fixtureDatesDiv.append(button);
@@ -88,6 +85,7 @@ function displayFixtureDates(fixtures) {
 }
 
 function displayFixturesForDate(buttonValue, buttonText) {
+	console.log("Starting displayFixturesForDate");
 	// Set the heading for the fixture date
 	fixtureDisplayDateHeading.innerText = buttonText;
 
@@ -96,44 +94,50 @@ function displayFixturesForDate(buttonValue, buttonText) {
 		buttonValue.indexOf("-") + 1
 	)}`;
 
+	// Get the fixtures that match the date selected
 	const fixtures = fixturesObj.filter((obj) => obj.league.leagueRound === date);
-	console.log(fixtures);
+	// console.log(fixtures);
 
 	if (fixtures.length !== 0) {
+		// go through each fixture and create a div containing its information
 		for (let f of fixtures) {
+			// console.log(f);
 			const fixtureMatchInfoDiv = createFixtureContainer(f);
-			fixtureDatesDiv.appendChild(fixtureMatchInfoDiv);
+			// append div with fixture information to parent div
+			fixturesForDateDiv.appendChild(fixtureMatchInfoDiv);
 		}
 	}
+	console.log("Ending displayFixturesForDate");
 }
 
 function createFixtureContainer(f) {
+	console.log("Starting createFixtureContainer");
 	const { fixture, teams, goals, score } = f;
-	console.log(teams);
-	const { home, away } = teams;
+	const { home: homeTeamInfo, away: awayTeamInfo } = teams;
 
 	// create fixture match info div
 	const fixtureMatchInfoDiv = document.createElement("div");
 	fixtureMatchInfoDiv.classList.add("fixture-match-info-div");
 
 	// create divs for home team and away team
-	const homeTeamInfoDiv = createTeamInfoDiv(home, "home");
-	const awayTeamInfoDiv = createTeamInfoDiv(away, "away");
+	const homeTeamInfoDiv = createTeamInfoDiv(homeTeamInfo, "home");
+	const awayTeamInfoDiv = createTeamInfoDiv(awayTeamInfo, "away");
 
 	// create div for match info
 	const matchInfoDiv = createMatchInfoDiv(fixture, goals, score);
 
-	fixtureMatchInfoDiv.appendChild(
-		homeTeamInfoDiv,
-		matchInfoDiv,
-		awayTeamInfoDiv
-	);
+	fixtureMatchInfoDiv.append(homeTeamInfoDiv, matchInfoDiv, awayTeamInfoDiv);
 
+	console.log("Ending createFixtureContainer");
 	return fixtureMatchInfoDiv;
 }
 
 function createTeamInfoDiv(team, type) {
+	console.log("Starting createTeamInfoDiv");
+
 	const teamInfo = teamInfoObj.find((obj) => obj.teamID === team.teamID);
+	console.log(teamInfo);
+
 	const teamInfoDiv = document.createElement("div");
 	if (type === "home") {
 		teamInfoDiv.classList.add("homeTeam", "team");
@@ -149,10 +153,12 @@ function createTeamInfoDiv(team, type) {
 	teamNamePara.innerText = teamInfo.teamName;
 
 	teamInfoDiv.append(teamLogoImg, teamNamePara);
+	console.log("Ending createTeamInfoDiv");
 	return teamInfoDiv;
 }
 
 function createMatchInfoDiv(fixture, goals, score) {
+	console.log("Starting createMatchInfoDiv");
 	const matchStatusLong = fixture.status.long;
 	const matchStatusShort = fixture.status.short;
 	const dateLong = fixture.date;
@@ -167,23 +173,26 @@ function createMatchInfoDiv(fixture, goals, score) {
 	const timePara = document.createElement("p");
 	const time = dateLong.substring(
 		dateLong.indexOf("T") + 1,
-		dateLong.indexOf("T") + 5
+		dateLong.indexOf("T") + 6
 	);
-	timePara.innerText = time;
+	timePara.innerText = `${time} UTC`;
 
 	matchInfoDiv.append(datePara, timePara);
 
 	const statusPara = document.createElement("p");
 	if (matchStatusShort === "FT") {
-		statusPara.innerText = `FT - ${score.fullftime.home} : ${score.fulltime.away}`;
+		statusPara.innerText = `FT - ${score.fulltime.home} : ${score.fulltime.away}`;
 	} else if (matchStatusShort === "HT") {
 		statusPara.innerText = `HT - ${score.halftime.home} : ${score.halftime.away}`;
 	} else if (matchStatusShort === "CANC") {
 		statusPara.innerText = `Canceled`;
 	} else if (matchStatusShort === "PST") {
 		statusPara.innerText = `Postponed`;
+	} else if (matchStatusShort === "NS") {
+		statusPara.innerText = "Not Started";
 	}
 
 	matchInfoDiv.append(statusPara);
+	console.log("Ending createMatchInfoDiv");
 	return matchInfoDiv;
 }
