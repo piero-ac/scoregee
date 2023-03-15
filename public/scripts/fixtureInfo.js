85142838f2653eed85a761138dd0005eab448383
Piero Coronado <pacoronado01@gmail.com>
2023-03-15 14:36:44 -0400

    Add functionality based on lineup availability

--- public/scripts/fixtureInfo.js
+++ public/scripts/fixtureInfo.js
@@ -2,6 +2,7 @@ const matchInfoTitle = document.querySelector("#match-info-title");
 const fixtureLeague = document.querySelector("#fixture-league");
 const fixtureMatchInfoDiv = document.querySelector(".fixture-match-info-div");
 const matchInfoSection = document.querySelector("#match-info-sct");
+const matchLineupContainer = document.querySelector("#match-lineup");
 const quickInfoDiv = document.querySelector("#quick-info");
 const quickInfoData = document.querySelector("#quick-info-data");
 const leagueHomepageLink = document.querySelector("#league-hp-link");
@@ -43,36 +44,40 @@ fixtureInfoPromise
 	.then((data) => displayFixtureInfo(data))
 	.then((data) => {
 		const { lineup } = data;
-		const {
-			coach: hCoach,
-			formation: hFormation,
-			team: { name: hName },
-		} = lineup[0];
+		if (lineup.length === 0) {
+			matchLineupContainer.textContent = "Information is not available yet.";
+		} else {
+			const {
+				coach: hCoach,
+				formation: hFormation,
+				team: { name: hName },
+			} = lineup[0];
 
-		const {
-			coach: aCoach,
-			formation: aFormation,
-			team: { name: aName },
-		} = lineup[1];
+			const {
+				coach: aCoach,
+				formation: aFormation,
+				team: { name: aName },
+			} = lineup[1];
 
-		// Display the lineup subheaders
-		homeTeamCoach.innerText = `${hName.toUpperCase()} Coach: ${
-			hCoach.name
-		} Formation: ${hFormation}`;
-		awayTeamCoach.innerText = `${aName.toUpperCase()} Coach: ${
-			aCoach.name
-		} Formation: ${aFormation}`;
+			// Display the lineup subheaders
+			homeTeamCoach.innerText = `${hName.toUpperCase()} Coach: ${
+				hCoach.name
+			} Formation: ${hFormation}`;
+			awayTeamCoach.innerText = `${aName.toUpperCase()} Coach: ${
+				aCoach.name
+			} Formation: ${aFormation}`;
 
-		// Display home team starters
-		displayPlayers(lineup[0].startXI, homeTeamStarters);
+			// Display home team starters
+			displayPlayers(lineup[0].startXI, homeTeamStarters);
 
-		// Display away team starters
-		displayPlayers(lineup[1].startXI, awayTeamStarters);
+			// Display away team starters
+			displayPlayers(lineup[1].startXI, awayTeamStarters);
 
-		// Display home team subs
-		displayPlayers(lineup[0].substitutes, homeTeamSubs);
+			// Display home team subs
+			displayPlayers(lineup[0].substitutes, homeTeamSubs);
 
-		// Display away team subs
-		displayPlayers(lineup[1].substitutes, awayTeamSubs);
+			// Display away team subs
+			displayPlayers(lineup[1].substitutes, awayTeamSubs);
+		}
 	})
 	.catch((err) => console.log(err));
